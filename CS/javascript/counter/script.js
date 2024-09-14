4fb372f6f248cdaed8233ea3f4d28349c4f63e8c
asa-8 <ashton.holmes@hgs.hiddengeniusproject.org>
2024-09-13 17:44:31 -0700

    Update script.js

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,3 +1,11 @@
 function counter() {
-// Write your code below. Hint: you can finish it in about 3-5 lines.
+    // Write your code below. Hint: you can finish it in about 3-5 lines.
+
+    // Create a variable called counter_number, store the tag with the id 
+    //'counter_number' inside this variable  
+    counter_number = document.getElementById('counter-number');
+    
+    // Add 1 to the text of the tag
+    counter_number.innerHTML = parseInt(counter_number.innerHTML)+1;
+
 }
